d021b518c9a7e82b2ce913c45d8b9d2cad16639c
smallpotato <smallpotato6035@gmail.com>
2024-03-14 15:30:46 +0100

    implement fetch data from db

--- src/backend/database.js
+++ src/backend/database.js
@@ -1,5 +1,94 @@
 const mongoose = require('mongoose')
 
+const movieSchema = new mongoose.Schema({
+    plot:{
+        type:String
+    } ,
+    genres:[{
+        type:String
+    }],
+    runtime:{
+        type:Number
+    },
+    cast:[{
+        type: String
+    }],
+    num_mflix_comments:{
+        type: Number
+    },
+    title:{
+        type: String
+    },
+    countries:[{
+        type: String
+    }],
+    released:{
+        type: Date
+    },
+    directors:[{
+        type: String
+    }],
+    rated:{
+        type: String
+    },
+    awards:{
+        wins:{
+            type: Number
+        },
+        nominations: {
+            type: Number
+        },
+        text: {
+            type: String
+        }
+    },
+    lastupdated:{
+        type: String
+    },
+    year: {
+        type: Number
+    },
+    imdb:{
+        rating: {
+            type: Number
+        },
+        votes:{
+            type: Number
+        },
+        id:{
+            type: Number
+        }
+    },
+    type: {
+        type: String
+    },
+    tomatoes:{
+        viewer:{
+            rating: {
+                type: Number
+            },
+            numReviews:{
+                type: Number
+            },
+            meter:{
+                type: Number
+            }
+        },
+        lastupdated: {
+            type:Date
+        }
+    }
+
+
+}, {collection: 'movies'})
+
+const movie = mongoose.model('Movies', movieSchema, 'movies')
+
+async function findAll(model) {
+    let movieResult = await model.find()
+    return movieResult
+}
+
 function createDBConnection() {
 
     mongoose.connect(process.env.MONGO_URI)
@@ -7,4 +96,8 @@ function createDBConnection() {
         .catch(err => console.error('Could not connect to database.', err))
 }
 
-module.exports = createDBConnection
+async function getMovies() {
+    return await findAll(movie)
+}
+    
+module.exports = {createDBConnection, getMovies}
--- src/backend/server.js
+++ src/backend/server.js
@@ -3,15 +3,21 @@ const app = express()
 const dotenv = require('dotenv')
 dotenv.config()
 
-const createDBConnection = require('./database')
+const {createDBConnection, getMovies} = require('./database')
 
-// Routes
+// *** Routes *** //
 
 app.get('/', (req, res) => {
     res.send('Hello, node!')
 })
 
+app.get('/movies', async (req, res) => {
+    let movies = await getMovies()
+    res.json(movies)
+})
+
 app.listen(3000, ()=>{
     console.log('Node API is running on port 3000')
     createDBConnection()
+
 })
