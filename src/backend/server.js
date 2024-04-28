3ec222c18679ecf9c64b5d437f24ca97f40208cf
Cameron Toth <smallpotato6035@gmail.com>
2024-04-28 22:47:23 +0200

    implent cors to serve css

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,28 +1,51 @@
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
+const cors = require('cors')
+const path = require('path')
+
 dotenv.config()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('', express.static(path.join(__dirname, '../frontend')))
+
+app.use(express.static('../frontend/styles', {
+    setHeaders: (res, path) => {
+        if (path.endsWith('.css')) {
+            res.setHeader('Content-Type', 'text/css')
+        }
+    }
+}))
 
 const {createDBConnection, getMovies, getMovieByTitle} = require('./database')
 
 // *** Routes *** //
 
+app.use(cors({
+    origin: '*',
+}))
+
 app.get('/', (req, res) => {
-    res.send('Hello, node!')
+    res.sendFile(path.join(__dirname, '../frontend/home.html'))
 })
 
 app.get('/movies', async (req, res) => {
     let movies = await getMovies()
+    console.log("getting movies")
     res.json(movies)
 })
 
 app.get('/movies/:title', async (req, res) => {
     let movie = await getMovieByTitle(req.params.title)
+    //populate the html with correct data and return it
     res.json(movie)
 })
 
-app.listen(3000, ()=>{
-    console.log('Node API is running on port 3000')
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Node API is running on port ${process.env.PORT}`)
     createDBConnection()
 
 })
