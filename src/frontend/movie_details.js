907d7868370532efbda5399b836cdbc4544445b4
smallpotato <smallpotato6035@gmail.com>
2024-05-05 11:43:06 +0200

    add cinema selector to movie details

--- src/frontend/movie_details.js
+++ src/frontend/movie_details.js
@@ -1,12 +1,19 @@
 let movie
 let showtimes = []
+let cinemas = []
 let selectedCinemaId
+let selectedCinemaName
 
 async function getJsonDataFromEndpoint(APIendpoint) {
     const response = await fetch(`../${APIendpoint}`)
     const jsonData = await response.json()
     //console.log(jsonData)
     return jsonData
+}
+
+function selectCinema() {
+    selectedCinemaId = 1 // makes not null
+    renderShowtimes()
 }
 
 function renderMovieDetails() {
@@ -44,7 +51,7 @@ function renderShowtimes() {
             <div class = "row my-2"> `
         times.forEach(showtime => {
             //if(showtime.theater_id == selectedCinemaId || selectedCinemaId === '') { //only render showtimes for current cinema
-                htmlToRender+= `
+            htmlToRender+= `
                 <div class = "col-auto ">
                     <input type="radio" class="btn-check" name="options-outlined" id="primary-outlined-${day}-${showtime}" autocomplete="off">
                     <label class="btn btn-outline-primary" for="primary-outlined-${day}-${showtime}">${showtime}</label>
@@ -52,15 +59,38 @@ function renderShowtimes() {
                 `
             //}
         })
-        htmlToRender += `</div><hr>`
+        htmlToRender += '</div><hr>'
     }
     showtimesDiv.innerHTML += htmlToRender
 }
 
+function renderCinemas() {
+    const htmlDiv = document.getElementById('js-cinemaslist')
+    let htmlToRender = ''
+    let lastCity = ''
+    cinemas.forEach(cinema => {
+        if(lastCity != cinema.city) {     //first element
+            htmlToRender += `<hr><option id=${cinema._id} value="${cinema.name}">${cinema.city}</option>`
+        } 
+        lastCity = cinema.city
+        htmlToRender += `
+                <option id=${cinema._id} value="${cinema.name}" class="cinema-list-element"> 
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp${cinema.name} (${cinema.city})
+                </option>
+            `
+    })
+    htmlDiv.innerHTML += htmlToRender
+
+    htmlDiv.addEventListener('change', selectCinema, false)
+
+}
+
 async function init() {
-    const movieTitle = sessionStorage.getItem("selectedMovie")
-    selectedCinemaId = sessionStorage.getItem("selectedCinemaId")
+    const movieTitle = sessionStorage.getItem('selectedMovie')
+    selectedCinemaId = sessionStorage.getItem('selectedCinemaId')
     movie = await getJsonDataFromEndpoint(`movies/${movieTitle}/details`)
+    cinemas = await getJsonDataFromEndpoint('cinemas')
+
     //showtimes = await getJsonDataFromEndpoint(`showtimes/movies/${movie._id}`)
     showtimes = {
         'Today' : ['11:30', '17:30', '19:30', '21:00'],
@@ -68,11 +98,16 @@ async function init() {
         'Monday' : ['17:30', '19:00'],
         'Tuesday' : ['17:30', '21:00'],       
         'Wednesday' : ['21:00'],
-        "Thursday" : ['17:30', '19:30', '21:00']
+        'Thursday' : ['17:30', '19:30', '21:00']
     }
 
     renderMovieDetails()
-    renderShowtimes()
+    renderCinemas()
+    if(selectedCinemaId) {
+        document.getElementById(selectedCinemaId).selected = 'selected'
+        renderShowtimes()
+    } 
+   
 }
 
 init()
